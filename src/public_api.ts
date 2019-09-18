b2a97c367e6a9c541688e18d69e58f4f74ae838b
tungpt <thanhtung.uet@gmail.com>
2019-09-18 09:55:11 +0700

    First commit

---
+++ src/public_api.ts
@@ -0,0 +1,7 @@
+export {FlatAddToQueueButton} from './icons/FlatAddToQueueButton';
+export {FlatBox} from './icons/FlatBox';
+export {FlatBudgetCalculator} from './icons/FlatBudgetCalculator';
+export {FlatProject} from './icons/FlatProject';
+export {FlatTeamwork} from './icons/FlatTeamwork';
+export {FlatUserAccountBox} from './icons/FlatUserAccountBox';
+export {FlatWalletFilledMoneyTool} from './icons/FlatWalletFilledMoneyTool';
---
+++ transform.ts
@@ -0,0 +1,29 @@
+const lodash = require('lodash');
+const path = require('path');
+const fs = require('fs');
+const childProcess = require('child_process');
+
+const fileList: string[] = childProcess.execSync('echo src/assets/icons/*.svg', {
+  encoding: 'utf-8',
+})
+  .trim()
+  .split(/\s+/);
+
+let publicContent: string = '';
+fileList.forEach((filename: string) => {
+  const name: string = lodash.snakeCase(
+    `flat_${filename.split('/').splice(-1)[0].replace('.svg', '')}`,
+  );
+  let content: string = `import {IconDefinition} from '@ant-design/icons-angular';\n\n`;
+  const iconClassName: string = `${name.charAt(0).toUpperCase()}${lodash.camelCase(name).substr(1)}`;
+  content =
+    `${content}export const ${iconClassName}: IconDefinition = {
+  name: '${name}',
+  theme: 'outline',
+  icon: \`${fs.readFileSync(filename, {encoding: 'utf-8'}).trim()}\`,
+};\n`;
+  fs.writeFileSync(`src/icons/${iconClassName}.ts`, content);
+  publicContent = `${publicContent}export {${iconClassName}} from './icons/${iconClassName}';\n`;
+});
+
+fs.writeFileSync('src/public_api.ts', publicContent);
